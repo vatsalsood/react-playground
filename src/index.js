3ed7eb48a32f534bc3fcce8ee4971e9d52e0e5df
Vatsal Sood <vatsalsood@dal.ca>
2020-08-11 13:47:22 -0300

    usestate example

--- src/index.js
+++ src/index.js
@@ -2,26 +2,67 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
+const NotesApp = () => {
 
-  return (
-    <div>
-      Counter: {count}
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addNotes = e => {
+    e.preventDefault();
+    setNotes([...notes, { title, body }]);
+    setTitle('');
+    setBody('');
+  }
+
+  const removeNote = currentNote => {
+    setNotes(notes.filter(note => { return note.title !== currentNote.title }));
+  }
+
+  return (<div>
+    <h3>Notes</h3>
+    {notes.map(note => {
+      return (
+        <ul key={note.title}>
+          <li >
+            <h4>{note.title}</h4>
+            <p>{note.body}</p>
+          </li>
+          <button onClick={(e) => { e.preventDefault(); removeNote(note) }}>x</button>
+        </ul>
+      )
+    })}
+    <form onSubmit={addNotes}>
+      <input type="text" onChange={(e) => { setTitle(e.target.value) }} value={title}></input>
       <br />
-      <button onClick={() => { setCount(count + 1) }}>+</button>
-      <button onClick={() => { setCount(count - 1) }}>-</button>
-      <button onClick={() => { setCount(props.count) }}>Reset</button>
-    </div>);
+      <textarea type="textarea" onChange={(e) => { setBody(e.target.value) }} value={body}></textarea>
+      <br />
+      <button>Add To form</button>
+    </form>
+  </div>)
 }
 
-App.defaultProps = {
-  count: 0
-}
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count);
+
+//   return (
+//     <div>
+//       Counter: {count}
+//       <br />
+//       <button onClick={() => { setCount(count + 1) }}>+</button>
+//       <button onClick={() => { setCount(count - 1) }}>-</button>
+//       <button onClick={() => { setCount(props.count) }}>Reset</button>
+//     </div>);
+// }
+
+// App.defaultProps = {
+//   count: 0
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App count={10} />
+    <NotesApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
